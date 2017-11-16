cac4e77131bab30c3106e8642fb78d06ad17cbd8
Lee Raj <lerajk44@gmail.com>
2017-11-15 22:43:46 -0500

    deploymentchanges

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,6 @@ var SmartToken = artifacts.require("./SmartToken.sol");
 module.exports = function(deployer) {
 
 	//deployer.deploy(SmartToken, 1000000000000, "GN Compass", 18, "GNCT");
-	deployer.deploy(SmartToken, "GNCT", 18 , 1000000000000);
+	deployer.deploy(SmartToken, "GN Compass", "GNCT", 18 , 1000000000000);
   
 };
