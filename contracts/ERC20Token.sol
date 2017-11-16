aeffd3e8e5e49c6264b5985279780012cb1fa73e
Lee Raj <lerajk44@gmail.com>
2017-11-15 23:03:51 -0500

    ERC20modify

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -24,13 +24,13 @@ contract ERC20Token is IERC20Token, Utils {
         @param _symbol      token symbol
         @param _decimals    decimal points, for display purposes
     */
-    function ERC20Token(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply) {
+    function ERC20Token(string _name, string _symbol, uint8 _decimals) {
         require(bytes(_name).length > 0 && bytes(_symbol).length > 0); // validate input
 
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        totalSupply = _totalSupply;
+
     }
 
     /**
